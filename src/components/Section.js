4f4143b0bd6f9bfbaca77023599af2a365aaabe4
Cyrille Perois <cperois@outlook.com>
2019-06-15 00:39:58 +0200

    feat(Section): Expose only skewed prop

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,158 +1,76 @@
 import React from 'react'
-import { css } from '@emotion/core'
-import { withTheme } from 'emotion-theming'
+import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
 
-function isSkewedTop(props) {
-  return props.skewed || props.skewedTop
-}
+export const SectionTitle = styled.h1(
+  {
+    fontSize: '1.5rem',
 
-function isSkewedBottom(props) {
-  return props.skewed || props.skewedBottom
-}
+    '&::after': {
+      content: '""',
+      display: 'block',
+      marginTop: '0.5rem',
+      height: 3,
+      width: '3rem',
+    }
+  },
+  props => ({
+    '&::after': {
+      backgroundColor: props.theme.main
+    }
+  })
+)
 
-function isSkewed(props) {
-  return isSkewedTop(props) || isSkewedBottom(props)
-}
-
-const sectionBase = css`
-  position: relative;
-`
-
-const sectionNormal = props => css`
-  background-color: ${props.theme.background};
-  color: inherit;
-`
-
-const sectionPrimary = props => css`
-  background-color: ${props.theme.main};
-  color: white;
-`
-
-const sectionLight = css`
-  background-color: white;
-  color: inherit;
-`
-
-const sectionPadded = css`
-  padding-top: 5rem;
-  padding-bottom: 5rem;
-`
-
-const sectionSkewed = css`
-  z-index: 1;
-`
-
-const sectionSkewedTop = css`
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: inherit;
-    transform-origin: 100% 100%;
-    transform: skewY(3deg);
-    z-index: -1;
+const getSectionBackgroundColor = (variant, theme) => {
+  switch (variant) {
+    case 'primary':
+      return theme.main
+    case 'light':
+      return 'white'
+    default:
+      return theme.background
   }
-`
-
-const sectionSkewedTopPadded = css`
-  padding-top: 2rem;
-`
-
-const sectionSkewedBottom = css`
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: inherit;
-    transform-origin: 0 0;
-    transform: skewY(3deg);
-    z-index: -1;
-  }
-`
-
-const sectionSkewedBottomPadded = css`
-  padding-bottom: 2rem;
-`
-
-function _Section(props) {
-  const { variant, skewed, skewedTop, skewedBottom, padded, ...rest } = props
-  return <section
-    css={[
-      sectionBase,
-      props.variant === 'normal' && sectionNormal(props),
-      props.variant === 'primary' && sectionPrimary(props),
-      props.variant === 'light' && sectionLight,
-      props.padded && sectionPadded,
-      isSkewed(props) && sectionSkewed,
-      isSkewedTop(props) && sectionSkewedTop,
-      isSkewedBottom(props) && sectionSkewedBottom,
-      props.padded && isSkewedTop(props) && sectionSkewedTopPadded,
-      props.padded && isSkewedBottom(props) && sectionSkewedBottomPadded
-    ].filter(Boolean)}
-    {...rest}
-  />
 }
 
-_Section.propTypes = {
+export const SectionContainer = styled.section(
+  {
+    position: 'relative',
+    paddingTop: '5rem',
+    paddingBottom: '5rem'
+  },
+  props => {
+    const styles = {
+      backgroundColor: getSectionBackgroundColor(props.variant, props.theme),
+      color: props.variant === 'primary' ? 'white' : 'inherit',
+    }
+
+    if (props.skewed) {
+      styles.zIndex = 0
+      styles.transformOrigin = '100% 0'
+      styles.transform = 'skewY(3deg)'
+      styles['& > *'] = {
+        transform: 'skewY(-3deg)'
+      }
+    }
+
+    return styles
+  }
+)
+
+SectionContainer.propTypes = {
   variant: PropTypes.oneOf(['normal', 'primary', 'light'])
 }
 
-_Section.defaultProps = {
+SectionContainer.defaultProps = {
   variant: 'normal'
 }
 
-export const Section = withTheme(_Section)
+export const Section = props => {
+  const { children, ...rest } = props
 
-const titleBase = props => css`
-  margin-top: 0;
-
-  &::after {
-    content: '';
-    display: block;
-    margin-top: 0.5rem;
-    height: 3px;
-    width: 3rem;
-    background-color: ${props.theme.main};
-  }
-`
-
-const titlePrimary = css`
-  color: white;
-
-  &::after {
-    background-color: currentColor;
-  }
-`
-
-function _SectionTitle(props) {
-  const { variant, children, ...rest } = props
-
-  return <h2
-    css={[
-      titleBase(props),
-      variant === 'primary' && titlePrimary
-    ]}
-    {...rest}
-  >
-    {children}
-  </h2>
-}
-
-export const SectionTitle = withTheme(_SectionTitle)
-
-export const SectionSeparator = () => {
   return (
-    <hr
-      css={{
-        width: '80%'
-      }}
-    />
+    <SectionContainer {...rest}>
+      {props.skewed ? <div>{children}</div> : children}
+    </SectionContainer>
   )
 }
