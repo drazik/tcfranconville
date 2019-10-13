8ddd1688de579d1237e182f602cd706b13922742
Cyrille Perois <cperois@outlook.com>
2019-10-13 19:48:16 +0200

    feat(home): Update content and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from '@emotion/styled'
 import Layout from '../components/Layout'
 import Wrapper from '../components/Wrapper'
 import { Section, SectionTitle } from '../components/Section'
@@ -12,12 +11,6 @@ import { Link } from 'gatsby'
 import { Text } from '../components/Text'
 import { Button } from '../components/Button'
 import SplashImage from '../components/SplashImage'
-
-const MapIframe = styled.iframe`
-  width: 100%;
-  height: 400px;
-  border: 0;
-`
 
 const Stack = props => {
   const { spacing, ...rest } = props
@@ -111,47 +104,63 @@ const IndexPage = () => {
             <SectionTitle variant="primary">
               Venir jouer au TCF
             </SectionTitle>
-            <Stack spacing="m">
-              <Text>
-                Le club est ouvert à toutes et à tous. Envie de taper la petite balle jaune en famille ou entre amis ? En simple, en double ? Nos courts sont disponibles à la réservation, même si vous n'ête pas (encore) adhérents.
-              </Text>
-              <Button
-                as={Link}
-                to="/reservation"
-                css={mq({
-                  marginLeft: 'auto',
-                  marginRight: 'auto',
-                  width: ['100%', '100%', '50%']
-                })}
-                  block
-              >
-                Réserver un court
-              </Button>
-              <Text>
-                Vous aimeriez jouer régulièrement ? Devenez adhérent(e) du TCF afin de bénéficier d'un accès illimité aux courts.
-              </Text>
-              <Button
-                as={Link}
-                to="/tarifs"
-                css={mq({
-                  marginLeft: 'auto',
-                  marginRight: 'auto',
-                  width: ['100%', '100%', '50%']
-                })}
-                  block
-              >
-                Découvrir la cotisation
-              </Button>
-              <Text>
-                Le TC Franconville est situé au 78 Avenue des Marais, à côté du stade municipal Jean Rolland. Un parking est disponible à cette adresse, ainsi qu'un autre au 110 rue des Pommiers Saulniers.
-              </Text>
-              <Text>
-                Vous pouvez nous contacter par téléphone au 01 75 40 75 20 ou par e-mail à l'adresse <a href="mailto:tennis.club.franconville@cegetel.net" css={{ wordWrap: 'break-word' }}>tennis.club.franconville@cegetel.net</a>.
-              </Text>
-              <MapIframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2617.57427100331!2d2.2175768160385503!3d48.9996586793017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6673fa2c4f21b%3A0x73fa94e076916ed!2sTennis%20Club%20de%20Franconville!5e0!3m2!1sfr!2sfr!4v1570894237189!5m2!1sfr!2sfr"
-              />
-            </Stack>
+            <div
+              css={theme => mq({
+                display: 'grid',
+                gridTemplateColumns: ['1fr', '1fr', 'repeat(2, 1fr)'],
+                gridGap: theme.spacing * 6
+              })}
+            >
+              <div>
+                <Stack spacing="m">
+                  <Text>
+                    Le club est ouvert à toutes et à tous. Envie de taper la petite balle jaune en famille ou entre amis ? En simple, en double ? Nos courts sont disponibles à la réservation, même si vous n'ête pas (encore) adhérents.
+                  </Text>
+                  <Button
+                    as={Link}
+                    to="/reservation"
+                    css={mq({
+                      marginLeft: 'auto',
+                      marginRight: 'auto'
+                    })}
+                    block
+                  >
+                    Réserver un court
+                  </Button>
+                  <Text>
+                    Vous aimeriez jouer régulièrement ? En devenant adhérent(e) du TC Franconville, vous pourrez profiter au maximum des infrastructures du club. Différentes possibilités s'offrent à vous : cotisation à l'année, pour 6 mois...
+                  </Text>
+                  <Button
+                    as={Link}
+                    to="/tarifs"
+                    css={mq({
+                      marginLeft: 'auto',
+                      marginRight: 'auto'
+                    })}
+                    block
+                  >
+                    Découvrir la cotisation
+                  </Button>
+                  <Text>
+                    Le TC Franconville est situé au 78 Avenue des Marais, à côté du stade municipal Jean Rolland. Un parking est disponible à cette adresse, ainsi qu'un autre au 110 rue des Pommiers Saulniers.
+                  </Text>
+                  <Text>
+                    Vous pouvez nous contacter par téléphone au 01 75 40 75 20 ou par e-mail à l'adresse <a href="mailto:tennis.club.franconville@cegetel.net">tennis.club.franconville@cegetel.net</a>.
+                  </Text>
+                </Stack>
+              </div>
+              <div>
+                <iframe
+                  css={mq({
+                    width: '100%',
+                    height: ['400px', '500px', '100%'],
+                    border: 0
+                  })}
+                  title="Carte"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2617.57427100331!2d2.2175768160385503!3d48.9996586793017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6673fa2c4f21b%3A0x73fa94e076916ed!2sTennis%20Club%20de%20Franconville!5e0!3m2!1sfr!2sfr!4v1570894237189!5m2!1sfr!2sfr"
+                ></iframe>
+              </div>
+            </div>
           </Stack>
         </Wrapper>
       </Section>
