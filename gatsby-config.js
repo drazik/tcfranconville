f0148acf5bd80926377955cb6be01864ad28e4cc
Cyrille Perois <cperois@outlook.com>
2019-10-26 19:01:02 +0200

    feat: Configure gatsby-source-facebook plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
+
 module.exports = {
   siteMetadata: {
     title: `TC Franconville`,
@@ -34,7 +36,17 @@ module.exports = {
         }
       }
     },
-    'gatsby-plugin-emotion'
+    'gatsby-plugin-emotion',
+    {
+      resolve: `gatsby-source-facebook`,
+      options: {
+        places: [`TCFranconvilleOfficiel`],
+        params: {
+          fields: 'posts { message, created_time, id, permalink_url }',
+        },
+        key: process.env.FB_TOKEN
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
