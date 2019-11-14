99262c1ff29c9e2d75d8ab02809a0237da685e34
Cyrille Perois <cperois@outlook.com>
2019-11-14 11:22:45 +0100

    feat(competition): Put page description in page title block

---
+++ src/components/PageDescription/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from '../Text'
+
+export const PageDescription = props => {
+  return (
+    <Text css={{ textAlign: 'center' }} {...props} />
+  )
+}
--- src/pages/competition.js
+++ src/pages/competition.js
@@ -5,38 +5,41 @@ import Wrapper from '../components/Wrapper'
 import { Text } from '../components/Text'
 import SEO from '../components/seo'
 import { PageTitle } from '../components/PageTitle'
+import { PageDescription } from '../components/PageDescription'
 import { Stack } from '../components/Stack'
 
 const CompetitionPage = () => {
   return (
     <Layout>
       <SEO title="Compétition" />
-        <Section skewed variant="primary">
-          <PageTitle>Compétition</PageTitle>
-        </Section>
+      <Section skewed variant="primary">
+        <Wrapper>
+          <Stack>
+            <PageTitle>Compétition</PageTitle>
+            <PageDescription>
+              Venez participer quel que soit votre âge et votre niveau à des compétitions par équipes en championnats ou en tournois individuellement.
+            </PageDescription>
+          </Stack>
+        </Wrapper>
+      </Section>
       <Section skewed>
         <Wrapper>
-          <Stack spacing="xl">
-            <Text>
-              Venez participer quel que soit votre âge et votre niveau à des compétitions par équipes en championnats ou en tournois individuellement
-            </Text>
-            <Stack spacing="l">
-              <SectionTitle>Jeunes</SectionTitle>
-              <Stack>
-                <Text>Championnat printemps (garçons-filles)</Text>
-                <Text>
-                  Tournois internes couleurs une fois par trimestre (balles évolutives)
-                </Text>
-                <Text>
-                  Tournoi découverte vert fin de saison sportive
-                </Text>
-                <Text>
-                  Tournoi interne du club (balles dures) pour les catégories U12, U14, U16, U18
-                </Text>
-                <Text>
-                  Tournoi OPEN
-                </Text>
-              </Stack>
+          <Stack spacing="l">
+            <SectionTitle>Jeunes</SectionTitle>
+            <Stack>
+              <Text>Championnat printemps (garçons-filles)</Text>
+              <Text>
+                Tournois internes couleurs une fois par trimestre (balles évolutives)
+              </Text>
+              <Text>
+                Tournoi découverte vert fin de saison sportive
+              </Text>
+              <Text>
+                Tournoi interne du club (balles dures) pour les catégories U12, U14, U16, U18
+              </Text>
+              <Text>
+                Tournoi OPEN
+              </Text>
             </Stack>
           </Stack>
         </Wrapper>
