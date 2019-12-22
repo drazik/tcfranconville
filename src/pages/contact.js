a1184e2552ed4136a724b13832c48ac92d13a60b
Cyrille Perois <cperois@outlook.com>
2019-12-22 21:31:14 +0100

    feat(contact): Add content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,17 +4,51 @@ import Wrapper from '../components/Wrapper'
 import { Text } from '../components/Text'
 import SEO from '../components/seo'
 import { PageTitle } from '../components/PageTitle'
+import { TwoCol } from '../components/TwoCol'
+import { Stack } from '../components/Stack'
+import { mq } from '../helpers/media-queries'
 
 const ContactPage = () => {
   return (
     <>
-      <SEO title="Compétition" />
-        <Section skewed variant="primary">
-          <PageTitle>Contact</PageTitle>
-        </Section>
+      <SEO title="Contact" />
+      <Section skewed variant="primary">
+        <PageTitle>Contact</PageTitle>
+      </Section>
       <Section skewed>
         <Wrapper>
-          <Text>TODO</Text>
+          <TwoCol align="center">
+            <Stack spacing="m">
+              <Text>
+                Le TC Franconville est situé au 78 Avenue des Marais, à côté du
+                stade municipal Jean Rolland. Un parking est disponible à cette
+                adresse, ainsi qu'un autre au 110 rue des Pommiers Saulniers.
+              </Text>
+              <Text>
+                Vous pouvez nous contacter par téléphone au
+                01&nbsp;75&nbsp;40&nbsp;75&nbsp;20 ou par e-mail à l'adresse
+                {' '}
+                <a href="mailto:tennis.club.franconville@cegetel.net">
+                  tennis.club.franconville@cegetel.net
+                </a>.
+              </Text>
+            </Stack>
+            <div>
+              <iframe
+                css={{
+                  width: '100%',
+                  height: 400,
+                  border: 0,
+
+                  [mq('s')]: {
+                    height: 500
+                  },
+                }}
+                title="Carte"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2617.57427100331!2d2.2175768160385503!3d48.9996586793017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6673fa2c4f21b%3A0x73fa94e076916ed!2sTennis%20Club%20de%20Franconville!5e0!3m2!1sfr!2sfr!4v1570894237189!5m2!1sfr!2sfr"
+              ></iframe>
+            </div>
+          </TwoCol>
         </Wrapper>
       </Section>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Section, SectionTitle } from '../components/Section'
 import teachingCardBackground from '../images/teaching-card.jpg'
 import competitionCardBackground from '../images/competition-card.jpg'
 import animationCardBackground from '../images/animation-card.jpg'
-import { mq as mqNew } from '../helpers/media-queries'
+import { mq } from '../helpers/media-queries'
 import Card from '../components/Card'
 import { Link } from 'gatsby'
 import { Text } from '../components/Text'
@@ -48,7 +48,7 @@ const IndexPage = ({ data }) => {
         <Wrapper css={{
           marginTop: -130,
 
-          [mqNew('s')]: {
+          [mq('s')]: {
             marginTop: -150
           }
         }}>
@@ -59,11 +59,11 @@ const IndexPage = ({ data }) => {
               gridColumnGap: '1rem',
               gridRowGap: '2rem',
 
-              [mqNew('m')]: {
+              [mq('m')]: {
                 gridTemplateColumns: 'repeat(3, 1fr)'
               },
 
-              [mqNew('l')]: {
+              [mq('l')]: {
                 gridColumnGap: '2rem'
               }
             }}
@@ -105,75 +105,42 @@ const IndexPage = ({ data }) => {
             <SectionTitle variant="primary">
               Venir jouer au TCF
             </SectionTitle>
-            <div
-              css={theme => ({
-                display: 'grid',
-                gridTemplateColumns: '1fr',
-                gridGap: theme.spacing * 6,
-
-                [mqNew('m')]: {
-                  gridTemplateColumns: 'repeat(2, 1fr)'
-                }
-              })}
-            >
-              <div>
-                <Stack spacing="m">
-                  <Text>
-                    Le club est ouvert à toutes et à tous. Envie de taper la petite balle jaune en famille ou entre amis ? En simple, en double ? Nos courts sont disponibles à la réservation, même si vous n'ête pas (encore) adhérents.
-                  </Text>
-                  <Button
-                    as={Link}
-                    to="/reservation"
-                    css={{
-                      marginLeft: 'auto',
-                      marginRight: 'auto'
-                    }}
-                    block
-                  >
-                    Réserver un court
-                  </Button>
-                  <Text>
-                    Vous aimeriez jouer régulièrement ? En devenant adhérent(e) du TC Franconville, vous pourrez profiter au maximum des infrastructures du club. Différentes possibilités s'offrent à vous : cotisation à l'année, saisonnière...
-                  </Text>
-                  <Button
-                    as={Link}
-                    to="/tarifs"
-                    css={{
-                      marginLeft: 'auto',
-                      marginRight: 'auto'
-                    }}
-                    block
-                  >
-                    Découvrir la cotisation
-                  </Button>
-                  <Text>
-                    Le TC Franconville est situé au 78 Avenue des Marais, à côté du stade municipal Jean Rolland. Un parking est disponible à cette adresse, ainsi qu'un autre au 110 rue des Pommiers Saulniers.
-                  </Text>
-                  <Text>
-                    Vous pouvez nous contacter par téléphone au 01 75 40 75 20 ou par e-mail à l'adresse <a href="mailto:tennis.club.franconville@cegetel.net">tennis.club.franconville@cegetel.net</a>.
-                  </Text>
-                </Stack>
-              </div>
-              <div>
-                <iframe
-                  css={{
-                    width: '100%',
-                    height: 400,
-                    border: 0,
-
-                    [mqNew('s')]: {
-                      height: 500
-                    },
-
-                    [mqNew('m')]: {
-                      height: '100%'
-                    }
-                  }}
-                  title="Carte"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2617.57427100331!2d2.2175768160385503!3d48.9996586793017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6673fa2c4f21b%3A0x73fa94e076916ed!2sTennis%20Club%20de%20Franconville!5e0!3m2!1sfr!2sfr!4v1570894237189!5m2!1sfr!2sfr"
-                ></iframe>
-              </div>
-            </div>
+            <Stack spacing="m">
+              <Text>
+                Le club est ouvert à toutes et à tous. Envie de taper la petite balle jaune en famille ou entre amis ? En simple, en double ? Nos courts sont disponibles à la réservation, même si vous n'ête pas (encore) adhérents.
+              </Text>
+              <Button
+                as={Link}
+                to="/reservation"
+                css={{
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                  maxWidth: 300
+                }}
+                block
+              >
+                Réserver un court
+              </Button>
+              <Text>
+                Vous aimeriez jouer régulièrement ? En devenant adhérent(e) du TC Franconville, vous pourrez profiter au maximum des infrastructures du club. Différentes possibilités s'offrent à vous : cotisation à l'année, saisonnière...
+              </Text>
+              <Button
+                as={Link}
+                to="/tarifs"
+                css={{
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                  maxWidth: 300
+                }}
+                block
+              >
+                Découvrir la cotisation
+              </Button>
+              <Text>
+                N'hésitez pas à <Link to="/contact">nous contacter</Link> si
+                besoin.
+              </Text>
+            </Stack>
           </Stack>
         </Wrapper>
       </Section>
