772ddbcf3ca7bdc44a435d1c1a2feae1648cb57c
Cyrille Perois <cperois@outlook.com>
2019-12-29 12:06:13 +0100

    feat(competition): Add some content

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,64 +1,82 @@
-import React, { useRef } from "react";
-import BaseSlider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useRef, forwardRef, useEffect, useState } from 'react'
+import Hammer from 'hammerjs'
+
+const SliderTrack = forwardRef((props, ref) => {
+  const { transform, animating, ...rest } = props
+
+  return (
+    <div
+      css={{
+        display: 'flex',
+        flexWrap: 'nowrap',
+        transition: animating ? 'transform 0.4s ease-in-out' : null,
+      }}
+      style={{ transform }}
+      ref={ref}
+      {...rest}
+    />
+  )
+})
 
 export const Slider = props => {
   const { children, ...rest } = props
-  const ref = useRef(null)
+  const trackRef = useRef(null)
+  const animationTimeoutRef = useRef(null)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [animating, setAnimating] = useState(false)
+  const [delta, setDelta] = useState(0)
 
-  const pause = () =>
-    ref.current && ref.current.slickPause && ref.current.slickPause()
+  useEffect(() => {
+    const goToSlide = index => {
+      if (index < 0) {
+        index = 0
+      } else if (index >= children.length) {
+        index = children.length - 1
+      }
+
+      setAnimating(true)
+      setDelta(0)
+      clearTimeout(animationTimeoutRef.current)
+      animationTimeoutRef.current = setTimeout(() => setAnimating(false), 400)
+      setCurrentIndex(index)
+    }
+
+    const onPan = e => {
+      if (e.isFinal) {
+        if (e.deltaX < 0) {
+          goToSlide(currentIndex + 1)
+        } else if (e.deltaX > 0) {
+          goToSlide(currentIndex - 1)
+        } else {
+          goToSlide(currentIndex)
+        }
+      } else {
+        setDelta(e.deltaX)
+      }
+    }
+
+    const manager = new Hammer.Manager(trackRef.current)
+    manager.add(new Hammer.Pan({ threshold: 0, pointers: 0 }))
+    manager.on('pan', onPan)
+
+    return () => {
+      manager.off('pan', onPan)
+    }
+  }, [currentIndex, children.length])
+
+  const transform = `translateX(calc(${-currentIndex * 100}% ${
+    delta < 0 ? '-' : '+'
+  } ${Math.abs(delta)}px))`
 
   return (
-    <BaseSlider
-      onSwipe={pause}
-      ref={ref}
-      {...rest}
-    >
-      {children}
-    </BaseSlider>
+    <div css={{ overflow: 'hidden' }} {...rest}>
+      <SliderTrack ref={trackRef} transform={transform} animating={animating}>
+        {children}
+      </SliderTrack>
+    </div>
   )
 }
 
 export const Slide = props => {
-  const {
-    backgroundImage,
-    text,
-    tabindex,
-    style,
-    as: Component,
-    ...rest
-  } = props
-
-  return (
-    <div tabIndex={tabindex} style={style}>
-      <Component
-        css={{
-          height: '400px',
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'bottom right',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textAlign: 'center',
-          paddingTop: '40px',
-          textDecoration: 'none'
-        }}
-        {...rest}
-      >
-        <div
-          css={{
-            fontSize: '2.5rem',
-            fontWeight: 'bold',
-            color: 'white',
-            textShadow: '1px 1px 2px rgba(0, 0, 0, 0.35)'
-          }}
-        >
-          {text}
-        </div>
-      </Component>
-    </div>
-  )
+  return <div css={{ width: '100%', flexShrink: 0 }} {...props} />
 }
--- src/pages/competition.js
+++ src/pages/competition.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Section, SectionTitle } from '../components/Section'
+import {
+  Section,
+  SectionTitle,
+  SectionTitleDetails,
+} from '../components/Section'
 import Wrapper from '../components/Wrapper'
 import { Text } from '../components/Text'
 import SEO from '../components/seo'
@@ -7,6 +11,27 @@ import { PageTitle } from '../components/PageTitle'
 import { PageDescription } from '../components/PageDescription'
 import { Stack } from '../components/Stack'
 import { TwoCol } from '../components/TwoCol'
+import { Slider as BaseSlider, Slide } from '../components/Slider'
+import Ratio from 'react-ratio'
+import { IllustrationCard } from '../components/IllustrationCard'
+
+const Slider = props => {
+  return (
+    <BaseSlider
+      css={{
+        boxShadow: '0 40px 30px -30px rgba(0, 0, 0, 0.5)',
+        borderRadius: '0.5rem',
+
+        '& img': {
+          verticalAlign: 'middle',
+          width: '100%',
+          height: '100%',
+        },
+      }}
+      {...props}
+    />
+  )
+}
 
 const CompetitionPage = () => {
   return (
@@ -17,51 +42,63 @@ const CompetitionPage = () => {
           <Stack>
             <PageTitle>Compétition</PageTitle>
             <PageDescription>
-              Venez participer quel que soit votre âge et votre niveau à des compétitions par équipes en championnats ou en tournois individuellement.
+              Venez participer quel que soit votre âge et votre niveau à des
+              compétitions par équipes en championnats ou en tournois
+              individuellement.
             </PageDescription>
           </Stack>
         </Wrapper>
       </Section>
-      <Section skewed>
+      <Section variant="light" skewed>
         <Wrapper>
           <Stack spacing="l">
-            <SectionTitle>Jeunes</SectionTitle>
+            <SectionTitle>Épreuves par équipe</SectionTitle>
+            <Text>
+              Tout au long de chaque saison sportive, des équipes de différents
+              âges et niveaux sont constituées pour participer à des
+              compétitions par équipe.
+            </Text>
             <TwoCol>
-              <Stack>
-                <Text>Championnat printemps (garçons-filles)</Text>
-                <Text>
-                  Tournois internes couleurs une fois par trimestre (balles évolutives)
-                </Text>
-                <Text>
-                  Tournoi découverte vert fin de saison sportive
-                </Text>
-                <Text>
-                  Tournoi interne du club (balles dures) pour les catégories U12, U14, U16, U18
-                </Text>
-                <Text>
-                  Tournoi OPEN
-                </Text>
+              <Stack spacing="l">
+                <SectionTitle>
+                  Championnat printemps
+                  <SectionTitleDetails>
+                    Filles / garçons / dames / messieurs
+                  </SectionTitleDetails>
+                </SectionTitle>
               </Stack>
-              <div />
             </TwoCol>
-          </Stack>
-        </Wrapper>
-      </Section>
-      <Section skewed variant="light">
-        <Wrapper>
-          <Stack spacing="l">
-            <SectionTitle>Seniors</SectionTitle>
             <TwoCol reversed>
-              <Stack>
-                <Text>Championnat Rème série (Dames-Messieurs)</Text>
-                <Text>Championnat +35 ans pour les Dames et Messieurs</Text>
-                <Text>Championnat +45 ans Dames</Text>
-                <Text>Championnat printemps : 3 équipes engagées pour les Dames et les Messieurs, dont une équipe Dame en Championnat de France</Text>
-                <Text>Tournoi journée</Text>
-                <Text>Tournoi interne</Text>
-                <Text>Tournoi OPEN</Text>
+              <Stack spacing="l">
+                <SectionTitle>
+                  Championnat 4ème série
+                  <SectionTitleDetails>Dames / messieurs</SectionTitleDetails>
+                </SectionTitle>
               </Stack>
-              <div />
+            </TwoCol>
+            <TwoCol>
+              <Stack spacing="l">
+                <SectionTitle>
+                  Championnat +35 ans
+                  <SectionTitleDetails>Dames / messieurs</SectionTitleDetails>
+                </SectionTitle>
+              </Stack>
+            </TwoCol>
+            <TwoCol reversed>
+              <Stack spacing="l">
+                <SectionTitle>
+                  Championnat +45 ans
+                  <SectionTitleDetails>Dames</SectionTitleDetails>
+                </SectionTitle>
+              </Stack>
+            </TwoCol>
+            <TwoCol>
+              <Stack spacing="l">
+                <SectionTitle>
+                  Championnat +55 ans
+                  <SectionTitleDetails>Messieurs</SectionTitleDetails>
+                </SectionTitle>
+              </Stack>
             </TwoCol>
           </Stack>
         </Wrapper>
@@ -69,16 +106,132 @@ const CompetitionPage = () => {
       <Section skewed>
         <Wrapper>
           <Stack spacing="l">
-            <SectionTitle>Seniors +</SectionTitle>
-            <TwoCol>
-              <Stack>
-                <Text>Championnat Messieurs +55 ans</Text>
-                <Text>Tournois Open Seniors +</Text>
-                <Text>Tournoi interne</Text>
-                <Text>Tournoi OPEN</Text>
-              </Stack>
-              <div />
-            </TwoCol>
+            <SectionTitle>Épreuves individuelles</SectionTitle>
+            <Text>
+              Le club organise aussi des compétitions individuelles pour tous
+              les âges et niveaux
+            </Text>
+            <Stack spacing="xl">
+              <TwoCol align="center">
+                <Stack spacing="l">
+                  <SectionTitle>Tournoi Open</SectionTitle>
+                  <Stack spacing="m">
+                    <Text>
+                      Chaque été le club organise son tournoi Open, au mois
+                      d'août. De nombreux(ses) joueur(euses) du club, du
+                      département ainsi que de la France entière y prennent
+                      part.
+                    </Text>
+                    <Text>
+                      Catégories : simples dames (senior, 35, 45, 55); simples
+                      messieurs (senior, 35, 45, 55, 65); filles/garçons (U12,
+                      U14, U16, U18).
+                    </Text>
+                    <Text>
+                      La 38ème édition du tournoi open du TCF aura lieu du n au
+                      m août 2020.
+                    </Text>
+                  </Stack>
+                </Stack>
+                <Slider>
+                  {['01', '02', '03', '04', '05'].map(name => (
+                    <Slide key={name}>
+                      <Ratio ratio={2048 / 1536}>
+                        <img
+                          src={require(`../images/competition/open/${name}.jpg`)}
+                          alt=""
+                        />
+                      </Ratio>
+                    </Slide>
+                  ))}
+                </Slider>
+              </TwoCol>
+              <TwoCol reversed align="center">
+                <Stack spacing="l">
+                  <SectionTitle>Tournoi Open hiver</SectionTitle>
+                  <Stack spacing="m">
+                    <Text>
+                      Organisé entre fin novembre et début décembre en semaine.
+                      Les matchs ont lieu en journée (premiers matchs à 9h,
+                      dernière rotation à 15h), du lundi au vendredi.
+                    </Text>
+                    <Text>
+                      Deux catégories : simple messieurs senior et simple dames
+                      senior.
+                    </Text>
+                    <Text>
+                      La prochaine édition du tournoi Open journée aura lieu en
+                      novembre 2020.
+                    </Text>
+                  </Stack>
+                </Stack>
+                <IllustrationCard
+                  image={require('../images/competition/open-journee/01.jpg')}
+                  ratio={956 / 717}
+                />
+              </TwoCol>
+              <TwoCol>
+                <Stack spacing="l">
+                  <SectionTitle>Tournoi Open Séniors+</SectionTitle>
+                  <Stack spacing="m">
+                    <Text>...</Text>
+                  </Stack>
+                </Stack>
+              </TwoCol>
+              <TwoCol reversed>
+                <Stack spacing="l">
+                  <SectionTitle>Tournoi interne</SectionTitle>
+                  <Stack spacing="m">
+                    <Text>
+                      Au printemps, le tournoi interne permet à tous(tes) les
+                      joueurs(euses) du club de jouer ensemble dans la
+                      convivialité et la bonne humeur qui caractérisent le TCF.
+                    </Text>
+                    <Text>
+                      Catégories : filles/garçons (U12, U16), dames (senior,
+                      +35), messieurs (senior, +35, +55).
+                    </Text>
+                    <Text>Le prochain tournoi interne aura lieu...</Text>
+                  </Stack>
+                </Stack>
+              </TwoCol>
+              <TwoCol align="center">
+                <Stack spacing="l">
+                  <SectionTitle>Tournois internes couleurs</SectionTitle>
+                  <Stack spacing="m">
+                    <Text>
+                      Une fois par trimestres nos enseignants organisent des
+                      tournois par couleurs pour les enfants du club. Les
+                      enfants y sont regroupés par niveaux et jouent avec des
+                      balles évolutives.
+                    </Text>
+                    <Text>
+                      Catégories : blanc, violet, rouge, orange, vert.
+                    </Text>
+                  </Stack>
+                </Stack>
+                <Slider>
+                  {['01', '02'].map(name => (
+                    <Slide key={name}>
+                      <Ratio ratio={1.33}>
+                        <img
+                          src={require(`../images/competition/interne-couleurs/${name}.jpg`)}
+                          alt=""
+                        />
+                      </Ratio>
+                    </Slide>
+                  ))}
+                </Slider>
+              </TwoCol>
+              <TwoCol reversed>
+                <Stack spacing="l">
+                  <SectionTitle>Tournoi découverte vert</SectionTitle>
+                  <Stack spacing="m">
+                    <Text>En fin de saison sportive</Text>
+                  </Stack>
+                </Stack>
+              </TwoCol>
+            </Stack>
           </Stack>
         </Wrapper>
       </Section>
