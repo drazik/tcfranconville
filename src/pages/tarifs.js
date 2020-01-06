e795f8eeb6b8fcae3cae77a3e8a35872fccaba61
Cyrille Perois <cperois@outlook.com>
2020-01-06 19:02:00 +0100

    fix(tarifs): Typos

--- src/pages/tarifs.js
+++ src/pages/tarifs.js
@@ -5,13 +5,13 @@ import { Text, Caption, Subtitle } from '../components/Text'
 import SEO from '../components/seo'
 import { Stack } from '../components/Stack'
 import { ExternalLink } from '../components/ExternalLink'
-import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 import {
   Table,
   TableHead,
   TableBody,
   TableRow,
-  TableCell
+  TableCell,
 } from '../components/Table'
 import { PageTitle } from '../components/PageTitle'
 
@@ -21,9 +21,7 @@ const TarifsPage = () => {
       <SEO title="Tarifs" />
       <Section padded skewed variant="primary">
         <Wrapper>
-          <PageTitle>
-            Tarifs 2019/2020
-          </PageTitle>
+          <PageTitle>Tarifs 2019/2020</PageTitle>
         </Wrapper>
       </Section>
       <Section padded skewed>
@@ -32,13 +30,22 @@ const TarifsPage = () => {
             <SectionTitle>Licence</SectionTitle>
             <Stack>
               <Text>
-                La licence Club est associée à votre adhésion à un club affilié à la FFT. Intégrer un club affilié, c'est la garantie de trouver une structure qui répond à vos attentes et facilite votre pratique.
+                La licence Club est associée à votre adhésion à un club affilié
+                à la FFT. Intégrer un club affilié, c'est la garantie de trouver
+                une structure qui répond à vos attentes et facilite votre
+                pratique.
               </Text>
               <Text>
-                La licence Club 2020 est valable du 1er septembre 2019 au 31 août 2020. Son tarif est mis en place par la FFT : 29 € pour les adultes, 20 € pour les 18 ans et moins.
+                La licence Club 2020 est valable du 1er septembre 2019 au 31
+                août 2020. Son tarif est mis en place par la FFT : 29 € pour les
+                adultes, 20 € pour les 18 ans et moins.
               </Text>
               <Text>
-                Être licencié vous procure de nombreux avantages. <ExternalLink href="https://www.fft.fr/se-licencier/toutes-les-licences">Rendez vous sur le site de la FFT pour en savoir plus</ExternalLink>.
+                Être licencié vous procure de nombreux avantages.{' '}
+                <ExternalLink href="https://www.fft.fr/se-licencier/toutes-les-licences">
+                  Rendez vous sur le site de la FFT pour en savoir plus
+                </ExternalLink>
+                .
               </Text>
             </Stack>
           </Stack>
@@ -50,13 +57,22 @@ const TarifsPage = () => {
             <SectionTitle>Cotisation</SectionTitle>
             <Stack>
               <Text>
-                La cotisation vous donne accès aux infrastructures du club et vous permet de réserver des courts afin de jouer avec d'autres adhérents ou avec des invités (moyennant 6€ par invitation).
+                La cotisation vous donne accès aux infrastructures du club et
+                vous permet de réserver des courts afin de jouer avec d'autres
+                adhérents ou avec des invités (moyennant 6€ par invitation).
               </Text>
               <Text>
-                Nous proposons différents tarifs afin que chacun puisse trouver une cotisation qui correspond le mieux à ses besoins. En cas de question, n'hésitez pas à <Link to="/contact">nous contacter</Link>.
+                Nous proposons différents tarifs afin que chacun puisse trouver
+                une cotisation qui correspond le mieux à ses besoins. En cas de
+                question, n'hésitez pas à{' '}
+                <Link to="/contact">nous contacter</Link>.
               </Text>
               <Text>
-                Vous pouvez prendre votre cotisation en ligne sur <ExternalLink href="https://tenup.fft.fr/club/57950106/offres">TenUp</ExternalLink>.
+                Vous pouvez prendre votre cotisation en ligne sur{' '}
+                <ExternalLink href="https://tenup.fft.fr/club/57950106/offres">
+                  TenUp
+                </ExternalLink>
+                .
               </Text>
               <div css={{ overflowX: 'auto' }}>
                 <Table>
@@ -91,92 +107,89 @@ const TarifsPage = () => {
                         </Stack>
                       </TableCell>
                       <TableCell colSpan="2" align="center">
-                        <Text>120€ + licence</Text></TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>
-                          <Stack spacing="xs">
-                            <Text>Semaine</Text>
-                            <Caption>du lundi au vendredi de 8h à 18h</Caption>
-                          </Stack>
-                        </TableCell>
-                        <TableCell align="center">
-                          <Text>220€ + licence</Text>
-                        </TableCell>
-                        <TableCell align="center">
-                          <Text>230€ + licence</Text>
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>
-                          <Text>Retraite sportive</Text>
-                        </TableCell>
-                        <TableCell colSpan="2" align="center">
-                          <Text>155€ + licence</Text>
-                        </TableCell>
-                      </TableRow>
-                      <TableRow variant="primary">
-                        <TableCell>
-                          <Text>Cotisation parentale</Text>
-                          <Caption>
-                            Venez jouer avec votre enfant autant de fois que vous le souhaitez !
-                          </Caption>
-                        </TableCell>
-                        <TableCell colSpan="2" align="center">
-                          <Text>66€ + licence</Text>
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>
-                          <Stack spacing="xs">
-                            <Text>Printemps/été</Text>
-                            <Caption>du 1er mars au 31 août</Caption>
-                          </Stack>
-                        </TableCell>
-                        <TableCell align="center">
-                          <Text>160€ + licence</Text>
-                        </TableCell>
-                        <TableCell align="center">
-                          <Text>170€ + licence</Text>
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>
-                          <Stack spacing="xs">
-                            <Text>Été adulte</Text>
-                            <Caption>du 1er juin au 31 août</Caption>
-                          </Stack>
-                        </TableCell>
-                        <TableCell align="center">
-                          <Text>90€ + licence</Text>
-                        </TableCell>
-                        <TableCell align="center">
-                          <Text>100€ + licence</Text>
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>
-                          <Stack spacing="xs">
-                            <Text>
-                              Été jeune
-                            </Text>
-                            <Caption>
-                              du 1er juin au 31 août
-                            </Caption>
-                            <Caption>
-                              &lt;25 ans et hors école de tennis
-                            </Caption>
-                          </Stack>
-                        </TableCell>
-                        <TableCell colSpan="2" align="center">
-                          <Text>40€ + licence</Text>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </div>
+                        <Text>120€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Stack spacing="xs">
+                          <Text>Semaine</Text>
+                          <Caption>du lundi au vendredi de 8h à 18h</Caption>
+                        </Stack>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Text>220€ + licence</Text>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Text>230€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Text>Retraite sportive</Text>
+                      </TableCell>
+                      <TableCell colSpan="2" align="center">
+                        <Text>155€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow variant="primary">
+                      <TableCell>
+                        <Text>Cotisation parentale</Text>
+                        <Caption>
+                          Venez jouer avec votre enfant autant de fois que vous
+                          le souhaitez !
+                        </Caption>
+                      </TableCell>
+                      <TableCell colSpan="2" align="center">
+                        <Text>66€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Stack spacing="xs">
+                          <Text>Printemps/été</Text>
+                          <Caption>du 1er mars au 31 août</Caption>
+                        </Stack>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Text>160€ + licence</Text>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Text>170€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Stack spacing="xs">
+                          <Text>Été adulte</Text>
+                          <Caption>du 1er juin au 31 août</Caption>
+                        </Stack>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Text>90€ + licence</Text>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Text>100€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Stack spacing="xs">
+                          <Text>Été jeune</Text>
+                          <Caption>du 1er juin au 31 août</Caption>
+                          <Caption>&lt;25 ans et hors école de tennis</Caption>
+                        </Stack>
+                      </TableCell>
+                      <TableCell colSpan="2" align="center">
+                        <Text>40€ + licence</Text>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </div>
               <Text>
-                Pour toute souscription à une cotisation anuelle, une invitation est offerte.
+                Pour toute souscription à une cotisation annuelle, une
+                invitation est offerte
               </Text>
             </Stack>
           </Stack>
@@ -188,7 +201,8 @@ const TarifsPage = () => {
             <SectionTitle>Cours adultes</SectionTitle>
             <Stack>
               <Text>
-                Les cours se déroulent sur 28 semaines. Le calendrier vous sera communiqué lors de l'inscription.
+                Les cours se déroulent sur 28 semaines. Le calendrier vous sera
+                communiqué lors de l'inscription
               </Text>
               <Table>
                 <TableHead>
@@ -235,7 +249,9 @@ const TarifsPage = () => {
                   </TableRow>
                   <TableRow>
                     <TableCell>
-                      <Text>Séance de 1h ou 1h15 encadrée par un DE et un AMT</Text>
+                      <Text>
+                        Séance de 1h ou 1h15 encadrée par un DE et un AMT
+                      </Text>
                     </TableCell>
                     <TableCell align="center">
                       <Text>13€</Text>
@@ -261,7 +277,9 @@ const TarifsPage = () => {
             <SectionTitle>École de tennis</SectionTitle>
             <Stack>
               <Text>
-                Les tarifs de l'école de tennis incluent le prix de la cotisation. Les cours se déroulent sur 28 semaines. Le calendrier vous sera communiqué lors de l'inscription.
+                Les tarifs de l'école de tennis incluent le prix de la
+                cotisation. Les cours se déroulent sur 28 semaines. Le
+                calendrier vous sera communiqué lors de l'inscription.
               </Text>
               <Table>
                 <TableHead>
@@ -276,9 +294,7 @@ const TarifsPage = () => {
                   <TableRow>
                     <TableCell>
                       <Text>Mini-tennis</Text>
-                      <Caption>
-                        1h, 2014-2015
-                      </Caption>
+                      <Caption>1h, 2014-2015</Caption>
                     </TableCell>
                     <TableCell align="center">
                       <Text>160€</Text>
@@ -348,16 +364,17 @@ const TarifsPage = () => {
             <Stack>
               <Subtitle tag="h2">Réduction conjoint</Subtitle>
               <Text>
-                Si votre conjoint prend aussi une cotisation annuelle, vous bénéficiez de 80€ de réduction sur la vôtre. Il ne vous reste plus qu'à le/la convaincre.
+                Si votre conjoint prend aussi une cotisation annuelle, vous
+                bénéficiez de 80€ de réduction sur la vôtre. Il ne vous reste
+                plus qu'à le/la convaincre.
               </Text>
               <Subtitle tag="h2">Réduction enfant</Subtitle>
               <Text>
-                Lors de l'inscription de plusieurs enfants, vous bénéficiez de 30€ de réduction par enfant (10€ pour le mini-tennis).
+                Lors de l'inscription de plusieurs enfants, vous bénéficiez de
+                30€ de réduction par enfant (10€ pour le mini-tennis).
               </Text>
               <Subtitle tag="h2">Forfait familial</Subtitle>
-              <Text>
-                À partir de 1200€...
-              </Text>
+              <Text>À partir de 1200€...</Text>
             </Stack>
           </Stack>
         </Wrapper>
@@ -365,15 +382,24 @@ const TarifsPage = () => {
       <Section padded variant="primary">
         <Wrapper>
           <Stack spacing="l">
-            <SectionTitle variant="primary">
-              Je soutiens mon club
-            </SectionTitle>
+            <SectionTitle variant="primary">Je soutiens mon club</SectionTitle>
             <Stack>
               <Text>
-                Vous avez la possibilité de donner un coup de pouce financier à votre club. Pour cela, vous pouvez faire un don. Pour chaque tranche de 15€, près de <strong>10€ sont déductibles de vos impôts sur le revenu</strong> et vous bénéficiez d'<strong>une remise immédiate de 5€ sur votre cotisation</strong>.
+                Vous avez la possibilité de donner un coup de pouce financier à
+                votre club. Pour cela, vous pouvez faire un don. Pour chaque
+                tranche de 15€, près de{' '}
+                <strong>
+                  10€ sont déductibles de vos impôts sur le revenu
+                </strong>{' '}
+                et vous bénéficiez d'
+                <strong>une remise immédiate de 5€ sur votre cotisation</strong>
+                .
               </Text>
               <Text>
-                En faisant un don, vous permettez au club de contenir la hausse des tarifs, mais également de mettre en place des projets permettant d'améliorer les infrastructures et les équipements à disposition des adhérents.
+                En faisant un don, vous permettez au club de contenir la hausse
+                des tarifs, mais également de mettre en place des projets
+                permettant d'améliorer les infrastructures et les équipements à
+                disposition des adhérents.
               </Text>
             </Stack>
           </Stack>
