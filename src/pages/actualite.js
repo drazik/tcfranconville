bb37d0693515c10aba771eab9f55f3a10bf0f580
Cyrille Perois <cperois@outlook.com>
2020-01-11 15:51:40 +0100

    feat(actualite): Add meta description

--- src/pages/actualite.js
+++ src/pages/actualite.js
@@ -4,28 +4,36 @@ import Wrapper from '../components/Wrapper'
 import SEO from '../components/seo'
 import { PageTitle } from '../components/PageTitle'
 import { graphql } from 'gatsby'
-import { Stack } from '../components/Stack'
+import { Stack } from '../components/Stack'
 import { ExternalLink } from '../components/ExternalLink'
 import { PageDescription } from '../components/PageDescription'
 import { Button } from '../components/Button'
 import { Post } from '../components/Post'
 
 const ActualitePage = ({ data }) => {
-  const posts = data.allFacebookPosts.edges.map(edge => edge.node).filter(post => post.message).slice(0, 10)
+  const posts = data.allFacebookPosts.edges
+    .map(edge => edge.node)
+    .filter(post => post.message)
+    .slice(0, 10)
 
   return (
     <>
-      <SEO title="Actualité" />
-        <Section skewed variant="primary">
-          <PageTitle>L'actu du club</PageTitle>
-          <PageDescription>
-            Votre club bouge. Retrouvez ici les dernières actualités.
-          </PageDescription>
-        </Section>
+      <SEO
+        title="Actualité"
+        description="Votre club bouge. Retrouvez ici les dernières actualités."
+      />
+      <Section skewed variant="primary">
+        <PageTitle>L'actu du club</PageTitle>
+        <PageDescription>
+          Votre club bouge. Retrouvez ici les dernières actualités.
+        </PageDescription>
+      </Section>
       <Section skewed>
         <Wrapper>
           <Stack spacing="xl">
-            {posts.map(post => <Post post={post} key={post.id} />)}
+            {posts.map(post => (
+              <Post post={post} key={post.id} />
+            ))}
             <div css={{ textAlign: 'center' }}>
               <Button
                 as={ExternalLink}
