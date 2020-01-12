7182730d478d698f905d66d8842d7302654b9c05
Cyrille Perois <cperois@outlook.com>
2020-01-12 18:37:21 +0100

    feat(SplashImage): Optimize background image

--- src/components/SplashImage.js
+++ src/components/SplashImage.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import image from '../images/splash.jpg'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 const SplashImage = props => {
-  const { component: Component, ...rest } = props
+  const { component: Component, children, ...rest } = props
+  const data = useStaticQuery(graphql`
+    query {
+      backgroundImage: file(relativePath: { eq: "splash.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
   return (
-    <Component
-      css={{
-        backgroundImage: `url(${image})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed'
-      }}
-      {...rest}
-    />
+    <Component css={{ position: 'relative' }} {...rest}>
+      <Img
+        fluid={data.backgroundImage.childImageSharp.fluid}
+        alt=""
+        css={{
+          position: 'absolute !important',
+          width: '100%',
+          height: '100%',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
+      {children}
+    </Component>
   )
 }
 
