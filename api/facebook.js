7ffb242db45c997ec2723f4dfbb83bce1bb65cf1
Cyrille Perois <cperois@outlook.com>
2020-01-15 20:14:27 +0100

    feat: Call zeit hook url when receiving a post request

--- api/facebook.js
+++ api/facebook.js
@@ -1,12 +1,17 @@
+const fetch = require('node-fetch')
+
 function handleSubscribeChallenge(req, res) {
   res.send(req.query['hub.challenge'])
 }
 
 function handleNewFacebookEvent(req, res) {
-  res.json({
-    query: req.query,
-    body: req.body,
-  })
+  const buildHookUrl =
+    'https://api.zeit.co/v1/integrations/deploy/Qmcr1WQiLaVyHmYCnzxqn27eW3wkKmJKxKzsxquNKykKsj/cqISe8qeZb'
+
+  fetch(buildHookUrl)
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json(err))
 }
 
 module.exports = (req, res) => {
