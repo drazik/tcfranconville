5a0e807d5d2f4fce5e031aeac86f1f13b5e2acaf
Cyrille Perois <cperois@protonmail.com>
2020-08-20 09:21:48 +0200

    feat(actualite): use FacebookFeed

--- src/pages/actualite.js
+++ src/pages/actualite.js
@@ -3,20 +3,10 @@ import { Section } from "../components/Section"
 import Wrapper from "../components/Wrapper"
 import SEO from "../components/seo"
 import { PageTitle } from "../components/PageTitle"
-import { graphql } from "gatsby"
-import { Stack } from "../components/Stack"
-import { ExternalLink } from "../components/ExternalLink"
 import { PageDescription } from "../components/PageDescription"
-import { Button } from "../components/Button"
-import { Post } from "../components/Post"
+import { FacebookFeed } from "../components/FacebookFeed"
 
 const ActualitePage = ({ data }) => {
-  // const posts = data.allFacebookPosts.edges
-  //   .map((edge) => edge.node)
-  //   .filter((post) => post.message)
-  //   .slice(0, 10)
-  const posts = []
-
   return (
     <>
       <SEO
@@ -31,20 +21,7 @@ const ActualitePage = ({ data }) => {
       </Section>
       <Section skewed>
         <Wrapper>
-          <Stack spacing="xl">
-            {posts.map((post) => (
-              <Post post={post} key={post.id} />
-            ))}
-            <div css={{ textAlign: "center" }}>
-              <Button
-                as={ExternalLink}
-                href="https://www.facebook.com/TCFranconvilleOfficiel"
-                variant="primary"
-              >
-                Voir plus d'actualités sur Facebook
-              </Button>
-            </div>
-          </Stack>
+          <FacebookFeed />
         </Wrapper>
       </Section>
     </>
