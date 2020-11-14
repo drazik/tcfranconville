bdcdffef5e5e0652b500f90139d3a99741453568
Cyrille Perois <cperois@protonmail.com>
2020-11-14 19:42:22 +0100

    feat: add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import "../styles/globals.css"
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import NextNprogress from "nextjs-progressbar"
+import { Favicon } from "../components/Favicon"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Favicon />
       <Header />
       <Component {...pageProps} />
       <Footer />
