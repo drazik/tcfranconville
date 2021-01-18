3530f9243ffdc720c3cdee9fae0e2aea10b3eb38
Cyrille Perois <cperois@protonmail.com>
2021-01-18 20:24:27 +0100

    feat(pages/index): get data from the API

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,117 +1,59 @@
 import { Wrapper } from "../components/Wrapper"
-import { Section, SectionTitle } from "../components/Section"
+import { BOSection, Section, SectionTitle } from "../components/Section"
 import cn from "classnames"
 import PropTypes from "prop-types"
 import Link from "next/link"
-import teachingBackground from "../img/index/teaching.jpg"
-import competitionBackground from "../img/index/competition.jpg"
-import animationBackground from "../img/index/animation.jpg"
 import { FacebookFeed } from "../components/FacebookFeed"
-import { Button } from "../components/Button"
 import { SEO } from "../components/SEO"
+import * as React from "react"
+import { fetchData, useApiMedia } from "../api"
 
-export default function Home() {
+export default function Home({
+  greetingsSection,
+  activityCardBackground,
+  competitionCardBackground,
+  teachingCardBackground,
+  section3,
+  newsSection,
+  ...props
+}) {
+  const activityBackgroundUrl = useApiMedia(activityCardBackground)
+  const competitionBackgroundUrl = useApiMedia(competitionCardBackground)
+  const teachingBackgroundUrl = useApiMedia(teachingCardBackground)
+
   return (
     <>
-      <SEO title="Accueil" />
-      <Section variant="brand" skewed>
-        <Wrapper>
-          <div className="space-y-8">
-            <SectionTitle>Bienvenue&nbsp;!</SectionTitle>
-            <div className="prose lg:prose-lg">
-              <p>
-                Vous souhaitez vous maintenir en forme, en famille, entre
-                amis&nbsp;?
-                <br />
-                Vous voulez progresser dans votre discipline sportive, le
-                tennis&nbsp;?
-                <br />
-                Vous recherchez un lieu de partage d'émotions&nbsp;?
-              </p>
-              <p>
-                Quels que soient votre âge, votre niveau, votre ambition, le TCF
-                vous offre&nbsp;:
-              </p>
-              <ul>
-                <li>
-                  des enseignements à haute valeur ajoutée, dynamiques,
-                  motivants
-                </li>
-                <li>des compétitions adaptées, individuelles ou par équipe</li>
-                <li>
-                  des animations placées sous le signe de la convivialité, du
-                  jeu et de la gastronomie&nbsp;!
-                </li>
-              </ul>
-            </div>
-          </div>
-        </Wrapper>
-      </Section>
+      <SEO
+        title={props.metadata.title}
+        description={props.metadata.description}
+      />
+      <BOSection {...greetingsSection} />
       <Section>
         <Wrapper>
           <div className="md:h-64 space-y-8 md:grid md:grid-cols-3 md:gap-8 md:space-y-0 lg:h-96">
             <ThemeCard
               title="Enseignement"
               href="/enseignement"
-              style={{ backgroundImage: `url(${teachingBackground})` }}
+              style={{ backgroundImage: `url(${teachingBackgroundUrl})` }}
             />
             <ThemeCard
               title="Compétition"
               href="/competition"
-              style={{ backgroundImage: `url(${competitionBackground})` }}
+              style={{ backgroundImage: `url(${competitionBackgroundUrl})` }}
             />
             <ThemeCard
               title="Animations"
               href="/animations"
-              style={{ backgroundImage: `url(${animationBackground})` }}
+              style={{ backgroundImage: `url(${activityBackgroundUrl})` }}
             />
           </div>
         </Wrapper>
       </Section>
-      <Section variant="brand" skewed>
+      <BOSection {...section3} />
+      <Section skewed={newsSection.skewed} color={newsSection.color}>
         <Wrapper>
           <div className="space-y-8">
-            <SectionTitle>Venir jouer au TCF</SectionTitle>
-            <div className="space-y-8 lg:grid lg:grid-cols-2 lg:gap-16 lg:space-y-0">
-              <div className="space-y-6">
-                <div className="prose">
-                  <p>
-                    Le club est ouvert à toutes et à tous. Envie de taper la
-                    petite balle jaune en famille ou entre amis&nbsp;? En
-                    simple, en double&nbsp;? Nos courts sont disponibles à la
-                    réservation, même si vous n'êtes pas (encore) adhérent.
-                  </p>
-                </div>
-                <Link href="/reservation" passHref>
-                  <Button component="a" className="w-full">
-                    Réserver un court
-                  </Button>
-                </Link>
-              </div>
-              <div className="space-y-6">
-                <div className="prose">
-                  <p>
-                    Vous aimeriez jouer régulièrement&nbsp;? En devenant
-                    adhérent(e) du TC Franconville, vous pourrez profiter au
-                    maximum des infrastructures du club. Différentes
-                    possibilités s'offrent à vous&nbsp;: cotisation à l'année,
-                    saisonnière...
-                  </p>
-                </div>
-                <Link href="/tarifs" passHref>
-                  <Button component="a" className="w-full">
-                    Découvrir la cotisation
-                  </Button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </Wrapper>
-      </Section>
-      <Section>
-        <Wrapper>
-          <div className="space-y-8">
-            <SectionTitle>L'actualité du club</SectionTitle>
+            <SectionTitle>{newsSection.title}</SectionTitle>
             <FacebookFeed />
           </div>
         </Wrapper>
@@ -140,4 +82,12 @@ ThemeCard.propTypes = {
   href: PropTypes.string.isRequired,
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
+}
+
+export async function getStaticProps() {
+  const data = await fetchData("/home")
+
+  return {
+    props: data,
+  }
 }
