43d11c7bc4653258153bd7bfdc4e98cc64252589
Cyrille Perois <cperois@protonmail.com>
2021-01-23 04:29:19 +0100

    feat(BOSection): accept additional children

--- front/components/Section/BOSection.js
+++ front/components/Section/BOSection.js
@@ -2,12 +2,13 @@ import { Section } from "./Section"
 import { SectionTitle } from "./SectionTitle"
 import { SectionContent } from "./SectionContent"
 
-export const BOSection = ({ skewed, color, title, content }) => {
+export const BOSection = ({ skewed, color, title, content, children }) => {
   return (
     <Section skewed={skewed} color={color}>
       <div className="space-y-8">
         {title ? <SectionTitle>{title}</SectionTitle> : null}
         <SectionContent content={content} />
+        {children}
       </div>
     </Section>
   )
