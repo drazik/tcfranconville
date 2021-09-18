6298e82ee53048dbd80f93b84f155a6a4583feea
Cyrille Perois <cperois@protonmail.com>
2021-09-18 08:15:32 +0200

    feat: remove info banner
    No important information at the moment

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -3,23 +3,12 @@ import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import NextNprogress from "nextjs-progressbar"
 import { Favicon } from "../components/Favicon"
-import { StickyBanner } from "../components/StickyBanner"
-import Link from "next/link"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Favicon />
       <Header />
-      <StickyBanner>
-        <p className="text-center">
-          <strong>Information :</strong> forum des associations et portes
-          ouvertes les 11 et 12 septembre{" "}
-          <Link href="/forum-associations-2021">
-            <a className="underline">En savoir plus</a>
-          </Link>
-        </p>
-      </StickyBanner>
       <Component {...pageProps} />
       <Footer />
       <NextNprogress
