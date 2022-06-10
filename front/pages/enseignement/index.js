9def4cc7842602a51667d2c98d6e213990c075a0
Cyrille Perois <cperois@protonmail.com>
2022-06-10 11:35:05 +0200

    feat(tarifs): add help dialog

--- front/pages/enseignement/index.js
+++ front/pages/enseignement/index.js
@@ -1,18 +1,16 @@
 import { SEO } from "../../components/SEO"
 import { Section, SectionTitle } from "../../components/Section"
 import { Wrapper } from "../../components/Wrapper"
-import { InfoBubble } from "../../components/Icon"
 import minitennisIllustration from "../../img/enseignement/minitennis.jpg"
 import junior1Illustration from "../../img/enseignement/junior1.jpg"
 import junior2Illustration from "../../img/enseignement/junior2.jpg"
 import adosIllustration from "../../img/enseignement/ados.jpg"
 import adultIllustration from "../../img/enseignement/adult.jpg"
-import PropTypes from "prop-types"
-import cn from "classnames"
 import Link from "next/link"
 import { Button } from "../../components/Button"
 import { TwoCol } from "../../components/TwoCol"
 import { Illustration } from "../../components/Illustration"
+import { Info } from "../../components/Info"
 
 export default function EnseignementPage() {
   return (
@@ -304,44 +302,4 @@ export default function EnseignementPage() {
       </Section>
     </>
   )
-}
-
-function Info({ children, className, ...props }) {
-  const r = 50
-  const d = 2 * r
-
-  return (
-    <div className={cn("relative", className)} {...props}>
-      <InfoBubble
-        className="absolute w-12 h-12 fill-current text-brand"
-        style={{ top: "-10px", left: "-10px" }}
-      />
-      <div className="relative px-6 pt-16 pb-8 overflow-hidden text-white rounded-xl">
-        <div
-          className="absolute"
-          style={{
-            width: d,
-            height: d,
-            boxShadow: "0 0 0 1000px #1a38b1",
-            top: -r,
-            left: -r,
-            borderRadius: "50%",
-            zIndex: "-1",
-          }}
-        />
-        <p
-          className="absolute font-bold transform -translate-y-1/2"
-          style={{ top: "25px", left: "calc(50px + 1rem)" }}
-        >
-          Le saviez-vous ?
-        </p>
-        {children}
-      </div>
-    </div>
-  )
-}
-
-Info.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
 }
