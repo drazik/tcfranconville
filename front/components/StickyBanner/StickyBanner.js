86d832a504a0d9e78fd9c9647f6f83bc05e40ed8
Cyrille Perois <cyrille.perois@w3r.one>
2024-06-22 13:17:23 +0200

    fix: z-index order between info banners and mobile menu

--- front/components/StickyBanner/StickyBanner.js
+++ front/components/StickyBanner/StickyBanner.js
@@ -3,12 +3,10 @@ import { Wrapper } from "../Wrapper"
 export const StickyBanner = ({ children }) => {
   return (
     <div
-      className="fixed bottom-0 left-0 w-full z-50 bg-brand text-white py-4 lg:bottom-auto lg:top-0"
+      className="fixed bottom-0 left-0 w-full z-10 bg-brand text-white py-4 lg:bottom-auto lg:top-0"
       style={{ boxShadow: "0 0 20px 5px rgba(0, 0, 0, 0.5)" }}
     >
-      <Wrapper>
-        {children}
-      </Wrapper>
+      <Wrapper>{children}</Wrapper>
     </div>
   )
 }
