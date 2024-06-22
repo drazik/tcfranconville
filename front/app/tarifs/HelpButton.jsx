2ec3665f7abba7898e3586ddf094bf77ebe8e1b5
Cyrille Perois <cyrille.perois@w3r.one>
2024-06-22 13:34:55 +0200

    feat: add close button on pricing help button

--- front/app/tarifs/HelpButton.jsx
+++ front/app/tarifs/HelpButton.jsx
@@ -40,6 +40,18 @@ const HelpDialog = ({ open, onClose }) => {
           <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
         <div className="fixed inset-0 overflow-y-auto">
+          <Transition.Child
+            as={"div"}
+            className={"sticky top-0 z-10 px-4 pt-2 flex justify-center"}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Button size={"medium"}>Fermer</Button>
+          </Transition.Child>
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
