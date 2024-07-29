ae6e9f14317e783bdc380dbf33f5f6533d56f421
Cyrille Perois <cyrille.perois@w3r.one>
2024-07-29 15:08:33 +0200

    feat: check banners client side

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -3,13 +3,9 @@ import { Header } from "../components/Header/Header"
 import { Footer } from "../components/Footer/Footer"
 import { Favicon } from "../components/Favicon"
 import { Analytics } from "@vercel/analytics/react"
-import { StickyBanner } from "../components/StickyBanner"
-import Link from "next/link"
-import { Button } from "../components/Button"
 import { ProgressBar } from "../components/ProgressBar"
-import { infoBanners } from "./data"
-import { isAfter, isBefore } from "date-fns"
 import type { ReactNode } from "react"
+import { Banners } from "./Banners"
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
@@ -43,29 +39,4 @@ export const metadata = {
   verification: {
     google: "sgP1a5KOzca0JDP7SLouQZMzj2LT8Aou5c-ViSnpF8o",
   },
-}
-
-function Banners() {
-  const now = new Date()
-
-  const infoBannersToShow = infoBanners.filter(
-    ({ dateStart, dateEnd }) =>
-      isAfter(now, dateStart) && isBefore(now, dateEnd)
-  )
-
-  return infoBannersToShow.map(({ id, cta, content }) => (
-    <StickyBanner key={id}>
-      <div className={"flex gap-10 justify-between items-center"}>
-        <div className={"grow"}>{content}</div>
-
-        {cta ? (
-          <div className={"shrink-0"}>
-            <Button asChild size="small">
-              <Link href={cta.href}>{cta.label}</Link>
-            </Button>
-          </div>
-        ) : null}
-      </div>
-    </StickyBanner>
-  ))
 }
