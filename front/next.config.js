0d1d1bf4bc056b51fcb5137454bf228259a4c13e
Cyrille Perois <cyrille.perois@w3r.one>
2026-07-16 10:42:45 +0200

    feat: add redirection from /inscription to form

--- front/next.config.js
+++ front/next.config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/inscription",
+        destination:
+          "https://forms.office.com/Pages/ResponsePage.aspx?id=t0kr1Y8MiU2MT_IFFzBuCOWqp7S1nYFCqqskZ7HCnTFUMFlQMkFYTUlEWEZZU1YwUUxIQjZDVFFHMi4u",
+        permanent: false,
+      },
+    ]
+  },
+}
